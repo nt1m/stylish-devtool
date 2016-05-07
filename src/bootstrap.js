97cfb104b0e740409099c69c271a8642d7e0168e
Tim Nguyen <ntim.bugs@gmail.com>
2016-05-07 20:15:01 +0200

    Fix remaining paths for release compat

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -13,7 +13,7 @@ try {
   let { LocalizationHelper } = require("devtools/client/shared/l10n");
   L10N = new LocalizationHelper(`${basePath}/locale/strings.properties`);
 } catch(e) {
-  // Firefox 44 to 48
+  // Firefox 44 to 47
   let {ViewHelpers} =
     require("resource://devtools/client/shared/widgets/ViewHelpers.jsm");
   L10N = new ViewHelpers.L10N(`${basePath}/locale/strings.properties`);
--- src/chrome/globals.js
+++ src/chrome/globals.js
@@ -1,14 +1,15 @@
 const basePath = "chrome://stylish-devtool";
 let {utils: Cu, interfaces: Ci, classes: Cc} = Components;
-let require, L10N;
+let {require} =
+    Cu.import("resource://devtools/shared/Loader.jsm", {});
+let L10N;
 try {
-  ({require} =
-    Cu.import("resource://devtools/shared/Loader.jsm", {}));
+  // Firefox 48+
   let { LocalizationHelper } = require("devtools/client/shared/l10n");
   L10N = new LocalizationHelper(`${basePath}/locale/strings.properties`);
 } catch(e) {
-  ({require} =
-    Cu.import("resource://gre/modules/devtools/Loader.jsm", {}).devtools);
-  let {ViewHelpers} = require("resource:///modules/devtools/ViewHelpers.jsm");
+  // Firefox 44 to 47
+  let {ViewHelpers} =
+    require("resource://devtools/client/shared/widgets/ViewHelpers.jsm");
   L10N = new ViewHelpers.L10N(`${basePath}/locale/strings.properties`);
 }
